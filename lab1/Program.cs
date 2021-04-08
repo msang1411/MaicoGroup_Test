2b548dd4e3447a2baeac8a5419432a529d4ec642
Pham Minh Sang <harley20005@gmail.com>
2021-04-08 10:51:21 +0700

    Finish

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 //
 //      Giải bài toán Fibonancci(Tối thiểu 3 cách).
@@ -15,18 +16,27 @@ namespace lab1
             //cach 1 dùng for
             Console.WriteLine("cach 1.");
             way1 w1 = new way1();
-            arrFibonancci = w1.CreateArrayFibonancci(1,1);
+            arrFibonancci = w1.CreateArrayFibonancci(0,1);
             Console.Write("Fibonancci: ");
             w1.PrintFibonancci(arrFibonancci);
             Console.WriteLine("\n\n-------------------------------------\n");
 
-            //cach 1 dùng de quy
+            //cach 2 dùng de quy
             Console.WriteLine("cach 2.");
             way2 w2 = new way2();
             arrFibonancci = w2.CreateArrayFibonancci();
             Console.Write("Fibonancci: ");
             w2.PrintFibonancci(arrFibonancci);
             Console.WriteLine("\n\n-------------------------------------\n");
+
+            //cach 3 dùng list
+            Console.WriteLine("cach 3.");
+            way3 w3 = new way3();
+            List<int> listFibonancci = w3.CreateListFibonacci();
+            Console.Write("Fibonancci: ");
+            w3.PrintFibonancciWithList(listFibonancci);
+            Console.WriteLine("\n\n-------------------------------------\n");
+
             Console.ReadKey();
         }
     }
