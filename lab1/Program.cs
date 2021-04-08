eda0df58b1affdece9c34fc4b97d9e4c52b7f8f1
Pham Minh Sang <harley20005@gmail.com>
2021-04-08 10:11:23 +0700

    Bai 1 phan 1

---
+++ lab1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+//
+//      Giải bài toán Fibonancci(Tối thiểu 3 cách).
+//
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Chỉ lấy 15 số trong chuỗi fibonancci
+            int[] arrFibonancci = new int[15];
+
+            //cach 1 dùng for
+            Console.WriteLine("cach 1.");
+            way1 w1 = new way1();
+            arrFibonancci = w1.CreateArrayFibonancci(1,1);
+            Console.Write("Fibonancci: ");
+            w1.PrintFibonancci(arrFibonancci);
+            Console.WriteLine("\n\n-------------------------------------\n");
+
+            //cach 1 dùng de quy
+            Console.WriteLine("cach 2.");
+            way2 w2 = new way2();
+            arrFibonancci = w2.CreateArrayFibonancci();
+            Console.Write("Fibonancci: ");
+            w2.PrintFibonancci(arrFibonancci);
+            Console.WriteLine("\n\n-------------------------------------\n");
+            Console.ReadKey();
+        }
+    }
+}
